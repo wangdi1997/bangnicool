c77cbb69005956d44a8e2518a509be3c6b5f7566
wangdi1997 <dibaowang@126.com>
2016-09-18 05:48:55 -0700

    finish web conn,start admin panal

--- bc-login/bc-login/src/main.js
+++ bc-login/bc-login/src/main.js
@@ -4,23 +4,22 @@ import VueRouter from 'vue-router'
 import App from './App'
 import Login from './components/Login'
 import Regist from './components/Regist'
+import Admin from './components/Admin'
 
 Vue.use(VueResource)
 Vue.use(VueRouter)
 
-/* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }
-})
-
 var router = new VueRouter()
+
 router.map({
   '/': {
     component: Login
   },
   '/regist': {
     component: Regist
+  },
+  '/admin': {
+    component: Admin
   }
 })
 
